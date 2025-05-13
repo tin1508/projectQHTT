98c0e72a9c2cc673eb4d773dbfe64c46d26155e8
tintin <nguyenhoang150805@gmail.com>
2025-05-13 19:34:57 +0700

    reset

--- ProjectQHTT/fearture.py
+++ ProjectQHTT/fearture.py
@@ -1,3 +1,2 @@
 #code chức năng
-def test():
-    print("Hello world!!!");
+
--- ProjectQHTT/main.py
+++ ProjectQHTT/main.py
@@ -1,6 +1,4 @@
 #làm xong tất cả các phần rồi import vào file này rồi chạy ứng dụng
 
-import fearture;
-
 if __name__ == "__main__":
-    fearture.test();
+    print("hello world!!");
