9ee0bfa3a98d95507bd0437ec5122ada1bb17528
tintin <nguyenhoang150805@gmail.com>
2025-06-03 21:46:53 +0700

    Final fix, add content to Readme.md and main.py

--- ProjectQHTT/main.py
+++ ProjectQHTT/main.py
@@ -2,3 +2,79 @@
 import GiaoDien
 if __name__ == "__main__":
     GiaoDien.showProgram();
+
+#1 số test case để test chương trình
+# 1.10a (có nghiệm)
+# min -x1 + x2
+# -x1 -2x2 <= 6
+# x1 - 2x2 <= 4
+# -x1 + x2 <= 1
+# x1 <=0 
+# x2 <= 0
+
+# 1.10b (vsn - hình chú thích là đoạn màu đen)
+# max x1 - x2
+# 3x1 + x2 >= 3
+# x1 + 2x2 >= 4
+# x1 - x2 <= 1
+# x1 <= 5
+# x2 <= 5
+
+# 1.10c (có nghiệm)
+# max 5x1 +6x2
+# x1 - 2x2 >= 2
+# -2x1 + 3x2 >= 2
+
+# 1.10d (có nghiệm)
+# min -2x1 -x2
+# x1 +2x2 <= 6
+# x1 - x2 >= 3
+# x1 >= 0
+# x2 >= 0
+
+# 1.12 (vn)
+# max 3x1 +2x2
+# 2x1 + x2 <= 2
+# 3x1 + 4x2 >= 12
+# x1 >= 0
+# x2 >= 0
+
+# test 2 pha:
+# max 2x1 - 6x2 
+# -x1 - x2 - x3 <= -2
+# 2x1 - x2 + x3 <= 1
+# x1 >= 0
+# x2 >= 0
+# x3 >= 0
+
+
+# test case đơn hình, bland, hình học:
+# max  4x1 + 5x2 
+# 2x1 + 2x2 <= 9
+# x1 <= 4
+# x2 <= 3
+# x1 >= 0
+# x2 >= 0
+
+# min -2x1 - x2
+# 3x1 + x2 <= 3
+# x1 >= 0
+# x2 >= 0
+
+# max 3x1 + 5x2
+# x1 + 2x2 <= 5
+# x1 <= 3
+# x2 <= 2
+# x1 >= 0
+# x2 >= 0
+
+# min -10x1 + 57x2 + 9x3 + 24x4 - 100x5
+# 0.5x1 - 5.5x2 - 2.5x3 + 9x4 + x5 <= 1
+# 0.5x1 - 1.5x2 - 0.5x3 + x4 + x5 <= 1
+# x1 + x5 <= 1
+# x5 <= 1
+# x1 >= 0
+# x2 >= 0
+# x3 >= 0
+# x4 >= 0
+# x5 >= 0
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # projectQHTT
-Nếu chưa có pip thì tải pip
+Yêu cầu: phải cài đặt python (version 3.4 trở lên), 
+Nếu chưa có pip thì tải pip(thường thì khi cài python sẽ có pip sẵn), tải pip bằng lệnh python get-pip.py, kiểm tra python bằng python --version, và pip bằng pip --version
+Sau đó thực hiện các bước dưới đây:
 B1: pip install pyqt6
 B2: pip install pyqt6-tools
-nếu như chưa có module matplot thì dùng lệnh pip install mathplot
-B3: pip install mathplot
-
+nếu như chưa có module matplot thì dùng lệnh pip để cài đặt tiếp
+B3: pip install matplotlib
+Sau các bước này thì vào file main.py và chạy chương trình và test thử (có sẵn 1 số test case trong file main.py)
 
